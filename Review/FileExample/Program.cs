abf8dc465b3ba4c6803653254bb3116ebdccf347
Matt Boensch <Matthew.D.Boensch-1@ou.edu>
2020-09-02 11:36:05 -0500

    Crrated the FileExample Project and successfully got it to read ch by ch

---
+++ Review/FileExample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace FileExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FilePath = @"C:\Random\Pride and Prejudice by Jane Austen.txt";
+
+            //string ContentsOfFile = File.ReadAllText(FilePath);
+
+            var Lines = File.ReadAllLines(FilePath);
+
+            for (int i = 0; i < Lines.Length; i++)
+            {
+                if (i % 5 == 0 && i != 0)
+                {
+                    string CurrentLine = Lines[i];
+                    if (CurrentLine.Contains("Chapter"))
+                    {
+                        Console.ReadKey();
+                    }
+                }
+                Console.WriteLine(Lines[i]);
+            }
+
+            Console.WriteLine(Lines);
+        }
+    }
+}
