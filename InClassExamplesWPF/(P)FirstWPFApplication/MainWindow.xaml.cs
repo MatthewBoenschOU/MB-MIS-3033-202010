e6224e4900aec2cd9a54725ce2d9c2d1a3584c43
Matt Boensch <Matthew.D.Boensch-1@ou.edu>
2020-09-11 18:09:21 -0500

    Completed Participation

---
+++ InClassExamplesWPF/(P)FirstWPFApplication/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _P_FirstWPFApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TxtBxName_MouseEnter(object sender, MouseEventArgs e)
+        {
+            TxtBxName.Text = "";
+        }
+
+        private void TxtBxBirthDate_MouseEnter(object sender, MouseEventArgs e)
+        {
+            TxtBxBirthDate.Text = "";
+        }
+
+        private void TxtBxBirthDate_MouseLeave(object sender, MouseEventArgs e)
+        {
+            TxtBxBirthDate.Text = "";
+        }
+
+        private void BttnAgeCalc_MouseEnter(object sender, MouseEventArgs e)
+        {
+            BttnAgeCalc.Background = new SolidColorBrush(Color.FromArgb(100, 100, 100, 100));
+        }
+
+        private void BttnAgeCalc_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ InClassExamplesWPF/IntroToWPFAttempt2/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IntroToWPFAttempt2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BttnCalc_MouseEnter(object sender, MouseEventArgs e)
+        {
+            BttnCalc.Background = Brushes.Aqua;
+        }
+
+        private void BttnCalc_Click(object sender, RoutedEventArgs e)
+        {
+            string Name, Age;
+            Name = TxtBxName.Text;
+            Age = TxtBxBirthDate.Text;
+                       
+
+            DateTime BirthDate = Convert.ToDateTime(Age);
+            var HowOld = DateTime.Now - BirthDate;
+            int Old = Convert.ToInt32(HowOld.TotalDays / 365);
+
+            MessageBox.Show($"Welcome {Name} who is {Old} years old");
+        }
+
+        private void BttnCalc_MouseLeave(object sender, MouseEventArgs e)
+        {
+            BttnCalc.Background = Brushes.Gray;
+        }
+    }
+}
