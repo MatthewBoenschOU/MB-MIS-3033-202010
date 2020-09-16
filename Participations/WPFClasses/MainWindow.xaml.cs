bd748852a27e5a9c4021d37d6e44cfce984bd448
Matt Boensch <Matthew.D.Boensch-1@ou.edu>
2020-09-16 12:00:27 -0500

    Completed Participation

---
+++ Participations/WPFClasses/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFClasses
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BttnEnter_Click(object sender, RoutedEventArgs e)
+        {
+            EntryForm NewEntry = new EntryForm();
+            NewEntry.Name = TxtBxName.Text;
+            NewEntry.Address = TxtBxAddress.Text;
+            NewEntry.ZipCode = Convert.ToInt32(TxtBxZip.Text);
+
+            LBEntries.Items.Add(NewEntry);
+
+            TxtBxName.Text = "Enter Name Here";
+            TxtBxAddress.Text = "Enter Address Here";
+            TxtBxZip.Text = "Enter ZipCode Here";
+        }
+
+        
+    }
+}
